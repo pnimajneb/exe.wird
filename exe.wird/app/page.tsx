1a5cdfc12ba140b73ff60058e044f75b6e4f4931
benjaminp <bpeters@posteo.de>
2024-06-03 11:03:42 +0200

    implementing select field via radix ui and shadcn

--- exe.wird/app/page.tsx
+++ exe.wird/app/page.tsx
@@ -1,9 +1,29 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../components/ui/select";
+
 import { Textarea } from "../components/ui/textarea";
 
 export default function Home() {
   return (
     <main>
-      <Textarea />
+      <div className="border-black border-2 rounded-[2px] w-1/2 h-full flex flex-col gap-4 p-12 m-12">
+        <Textarea className="p-4" />
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
     </main>
   );
 }
