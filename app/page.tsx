129741962fa2d4f9e681966c11b43995e3e10515
Stefan Wetzig <stefanwetzig@yahoo.de>
2024-06-20 16:54:05 +0200

    fix stuff after merge

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import CustomSelect from "../components/CustomSelect";
 import { Textarea } from "../components/ui/textarea";
 import { Button } from "../components/ui/button";
 import { text } from "stream/consumers";
+import { AppComponent } from "../components/app/app";
 
 type Font = {
   family: string;
@@ -24,101 +25,14 @@ export type FontOption = {
   label: string;
 };
 
-export async function getFonts(): Promise<FontOption[]> {
-  const GOOGLE_FONTS_API_KEY = process.env.GOOGLE_FONTS_API_KEY;
-  let fontOptions: FontOption[] = [];
-
-  try {
-    const response = await fetch(
-      `https://www.googleapis.com/webfonts/v1/webfonts?key=${GOOGLE_FONTS_API_KEY}`
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const data = await response.json();
-    fontOptions = data.items.map((font: Font) => ({
-      value: font.family,
-      label: font.family,
-    }));
-  } catch (error) {
-    console.error("Error fetching fonts:", error);
-  }
-
-  return fontOptions;
-}
-
 export type HomeProps = {
   fontOptions: FontOption[];
 };
 
 export default async function Home() {
-  const fontOptions = await getFonts();
-
-  const buttonsConfig = [
-    { icon: Upload, label: "Upload" },
-    { icon: Type, label: "Text" },
-    { icon: Grid2x2Check, label: "Pixel Tool" },
-  ];
-
-  const colorOptions = [
-    { value: "white", label: "Weiß", icon: { textcolor: "neutral-200" } },
-    {
-      value: "yellow",
-      label: "Gelb",
-      icon: { textcolor: "yellow-300", fillcolor: "yellow-400" },
-    },
-    {
-      value: "purple",
-      label: "Violett",
-      icon: { textcolor: "purple-300", fillcolor: "purple-400" },
-    },
-    {
-      value: "black",
-      label: "Schwarz",
-      icon: { textcolor: "black", fillcolor: "black" },
-    },
-  ];
-
   return (
     <main>
-      <div className="flex m-12 gap-4">
-        <div className="flex flex-col gap-12">
-          {buttonsConfig.map((button, index) => (
-            <ActionButton key={index} icon={button.icon} label={button.label} />
-          ))}
-        </div>
-        <div className="border-neutral-600 border-2 rounded-[2px] flex flex-col gap-4 p-12 w-1/3">
-          <Textarea className="p-4" />
-          <CustomSelect placeholder="Schriftart" options={fontOptions} />
-          <CustomSelect placeholder="Farbe" options={colorOptions} />
-          <div>
-            <p className="pb-2 pt-4">Textausrichtung</p>
-            <Button variant={"outline"} className="mr-2">
-              <AlignLeft />
-            </Button>
-            <Button variant={"outline"} className="mr-2">
-              <AlignRight />
-            </Button>
-            <Button variant={"outline"} className="mr-2">
-              <AlignCenter />
-            </Button>
-            <Button variant={"outline"} className="mr-2">
-              <AlignJustify />
-            </Button>
-          </div>
-          <div>
-            <p className="pb-2 pt-4">Zeilen- & Zeichenabstand</p>
-            <Button variant={"outline"}>
-              <WrapText />
-            </Button>
-          </div>
-        </div>
-        <div className="border-neutral-600 border-2 rounded-[2px] flex flex-col gap-4 p-12 w-1/3">
-          <p>Typed Text:</p>
-          <p>{}</p>
-        </div>
-      </div>
+      <AppComponent />
     </main>
   );
 }
--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -4,11 +4,21 @@ import { Button } from "./ui/button";
 type ActionButtonProps = {
   icon: React.ComponentType;
   label: string;
+  onClick?: () => void;
 };
 
-const ActionButton: React.FC<ActionButtonProps> = ({ icon: Icon, label }) => {
+const ActionButton: React.FC<ActionButtonProps> = ({
+  icon: Icon,
+  label,
+  onClick,
+}) => {
   return (
-    <Button className="flex flex-col gap-2" variant={"secondary"} size={"icon"}>
+    <Button
+      className="flex flex-col gap-2"
+      variant={"secondary"}
+      size={"icon"}
+      onClick={onClick}
+    >
       <Icon />
       <span className="text-[16px]">{label}</span>
     </Button>
